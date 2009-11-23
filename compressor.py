c8c9aa686eb677c0034c1fbd80aa40b555a88ac7
Paul Sowden <paul@paulsowden.com>
2009-11-22 22:49:39 -0800

    implement name substitution for lexically scoped names

--- compressor.py
+++ compressor.py
@@ -142,9 +142,9 @@ def compress(s):
 			str += 'else' + block(compress(s.elseblock))
 		return str
 	elif s.id == 'do':
-		pass
+		return 'do{%s}while(%s)' % (compress(s.block), compress(s.first))
 	elif s.id == 'while':
-		pass
+		return 'while(%s)%s' % (compress(s.first), block(compress(s.block)))
 	elif s.id == 'for':
 		if hasattr(s, 'iterator'):
 			for_loop = '%s in %s' % (compress(s.iterator), compress(s.object))
@@ -160,7 +160,7 @@ def compress(s):
 	elif s.id in ('return', 'throw'):
 		return alphabetic_operator_righthand(s, s.first)
 	elif s.id == 'with':
-		return 'with(%s)%s' % (compress(s.first), block(compress(s.second)))
+		return 'with(%s)%s' % (compress(s.first), block(compress(s.block)))
 	elif s.id == 'switch':
 		pass
 	elif s.id == 'try':
@@ -171,8 +171,8 @@ def compress(s):
 		return 'try{%s}%s%s' % (compress(s.block), catch, final)
 	elif s.id == 'function':
 		return 'function%s(%s)%s' % (
-			' ' + s.name if s.name else '',
-			','.join(s.params),
+			' ' + compress(s.name) if s.name else '',
+			','.join(compress(param) for param in s.params),
 			block(compress(s.block)))
 	#elif s.id == 'eval':
 	#	return 'eval'
@@ -181,6 +181,128 @@ def compress(s):
 		return compress(s.first) + '?' + compress(s.second) + ':' + compress(s.third)
 
 	raise RuntimeError, "unknown operation %s" % s.id
+
+class LexicalScope(object):
+	def __init__(self, parent, vars=set(), params=[], functions={}):
+		self.parent = parent
+		if parent:
+			parent.children.append(self)
+		self.children = []
+		self.vars = {}
+		for name, function in functions.items():
+			self.vars[name.value] = []
+		for var in vars:
+			self.vars[var] = []
+		for param in params:
+			self.vars[param.value] = []
+	def get_var(self, var):
+		if var in self.vars:
+			return self.vars[var]
+		elif self.parent:
+			return self.parent.get_var(var)
+		else:
+			return None
+
+
+def char_to_var_name(char):
+	prefix_chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_$'
+	all_chars = prefix_chars + '0123456789'
+
+	chars = prefix_chars
+	var = ''
+	while char >= 0:
+		var += chars[char%len(chars)]
+		char //= len(chars)
+		char -= 1
+		chars = all_chars
+	return var
+
+def rename_vars(ast):
+	scope = LexicalScope(None)
+	populate_scope(ast, ast.first, scope)
+
+	def flatten(scope):
+		vars = []
+		for item in scope.vars.items():
+			vars.append((scope,) + item)
+		for child in scope.children:
+			vars.extend(flatten(child))
+		scope.vars = {}
+		return vars
+
+	vars = flatten(scope)
+	vars.sort(lambda a, b: len(b[2]) - len(a[2]))
+	for scope, var, identifiers in vars:
+		char = 0
+		# TODO when checking for name collisions we should check all scopes
+		#      that references to this variable occur in for collisions
+		while scope.get_var(char_to_var_name(char)):
+			char += 1
+		scope.vars[char_to_var_name(char)] = True
+		for identifier in identifiers:
+			identifier.value = char_to_var_name(char)
+
+def populate_scope(parent, s, scope):
+	if isinstance(s, list) or s.id == '{': # block statement
+		if not isinstance(s, list):
+			s = s.block
+		for ast in s:
+			populate_scope(s, ast, scope)
+
+	elif s.id == '(identifier)':
+		if parent.id != '.' or s != parent.second:
+			var = scope.get_var(s.value)
+			if var is not None:
+				var.append(s)
+	elif s.id == '(array)':
+		for ast in s.first:
+			populate_scope(s, ast, scope)
+	elif s.id == '(object)':
+		for k, v in s.first:
+			populate_scope(s, v, scope)
+	elif s.id in ('new', '('):
+		populate_scope(s, s.first, scope)
+		if hasattr(s, 'params'):
+			for ast in s.params:
+				populate_scope(s, ast, scope)
+	elif s.id == 'var':
+		for ast in s.first:
+			populate_scope(s, ast, scope)
+	elif s.id == 'if':
+		populate_scope(s, s.first, scope)
+		populate_scope(s, s.block, scope)
+		if hasattr(s, 'elseblock'):
+			populate_scope(s, s.elseblock, scope)
+	elif s.id == 'for':
+		if hasattr(s, 'iterator'):
+			populate_scope(s, s.iterator, scope)
+			populate_scope(s, s.object, scope)
+		else:
+			populate_scope(s, s.initializer, scope)
+			populate_scope(s, s.condition, scope)
+			populate_scope(s, s.counter, scope)
+		populate_scope(s, s.block, scope)
+	elif s.id == 'try':
+		populate_scope(s, s.block, scope)
+		if hasattr(s, 'catchblock'):
+			catch_scope = LexicalScope(scope, params=[s.e])
+			populate_scope(s, s.catchblock, catch_scope)
+		if hasattr(s, 'finallyblock'):
+			populate_scope(s, s.finallyblock, scope)
+	elif s.id == 'function':
+		function_scope = LexicalScope(scope, s.vars, s.params, s.functions)
+		if s.name:
+			populate_scope(s, s.name, s.is_decl and function_scope or scope)
+		for param in s.params:
+			populate_scope(s, param, function_scope)
+		populate_scope(s, s.block, function_scope)
+	else:
+		if s.first:
+			populate_scope(s, s.first, scope)
+		if s.second:
+			populate_scope(s, s.second, scope)
+		if hasattr(s, 'block'):
+			populate_scope(s, s.block, scope)
 
 if __name__ == '__main__':
 	#js = '/home/paul/code/pyjs/jslint/jslint.js'
@@ -195,5 +317,6 @@ if __name__ == '__main__':
 	ast = parse_file(js)
 	#print ast
 
+	rename_vars(ast)
 	print compress(ast.first)
 
