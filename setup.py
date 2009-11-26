501531dd31460b9908b70e7d694a0670f1f126c2
Paul Sowden <paul@paulsowden.com>
2009-11-25 22:54:17 -0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from distutils.core import setup
+
+setup(
+	name='pyjs',
+	version='1.0',
+	description='Pure Python JavaScript',
+	author='Paul Sowden',
+	author_email='paul@paulsowden.com',
+	url='http://paulsowden.com/',
+	packages=['js'],
+)
+
+
