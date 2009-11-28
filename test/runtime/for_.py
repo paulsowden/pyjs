e1a1a230d751ef755b4257b2a6c8b091263169df
Paul Sowden <paul@paulsowden.com>
2009-11-28 14:33:12 -0800

    add a couple of for loop test cases

--- test/runtime/for_.py
+++ test/runtime/for_.py
@@ -11,9 +11,28 @@ class LabelledStatement(unittest.TestCase):
 				}
 			}""")
 
+class For(unittest.TestCase):
+	def test_for_in(self):
+		self.assertEqual(run(
+			"""var a = {a:1,b:2,c:3}, s = '';
+			for (var key in a) { s = s + key; }"""), 'abc')
+		self.assertEqual(run(
+			"""var a = {a:1,b:2,c:3}, s = '';
+			for (key in a) { s = s + key; }"""), 'abc')
+
+		self.assertEqual(run(
+			"""var a = {}; Object.prototype.a = 1; a.b = 1;
+			Object.prototype.c = 1; a.d = 1; var s = '';
+			for (var key in a) { s = s + key; }"""), 'bdac')
+
+	def test_for_loop(self):
+		self.assertEqual(run(
+			'for (var i = 0; i < 10; i = i + 1) ; i'), 10)
+
 def suite():
 	suite = unittest.TestSuite([
 		unittest.TestLoader().loadTestsFromTestCase(LabelledStatement),
+		unittest.TestLoader().loadTestsFromTestCase(For),
 		])
 	return suite
 
